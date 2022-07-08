dab7ceb5d76197d29ef83f05524dd0226d292fc8
Jairo Martins <jairo7martins@gmail.com>
2022-07-07 23:46:44 -0300

    atualização login/register

--- src/components/User/LoginUser.jsx
+++ src/components/User/LoginUser.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 
 import {Card, Col, Row, Container, Form, Button} from "react-bootstrap"
 
 
 const LoginUser = () => {
+
+    const [userEmail, setUserEmail] =  useState()
+    const [userPassword, setUserPassword] =  useState()
+
+
+    async function handleLogin (e){
+        e.preventDefault()
+
+        const response = await fetch('http://localhost:3000/auth/login',{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                userEmail,
+                userPassword
+            }),
+            
+        })
+
+        const data = await response.json()
+        console.log(data)
+    }
     return (  <>
     <Container className="text-center">
         <br/>
@@ -14,17 +37,19 @@ const LoginUser = () => {
                 <Card.Title>Entrar</Card.Title>
             </Card.Header>
             <Card.Body>
-                <Form>
+                <Form onSubmit={handleLogin}>
                     <Form.Control 
-                    autoComplete="off"
-                    placeholder="E-mail"/>
+                        autoComplete="off"
+                        onChange={(e)=>{setUserEmail(e.target.value)}}
+                        placeholder="E-mail"/>
                     <br/>
                     <Form.Control
-                    placeholder="senha"
-                    type="password"/>
+                        placeholder="senha"
+                        onChange={(e)=>{setUserPassword(e.target.value)}}
+                        type="password"/>
                     <br/>
                     <Button variant="success" type="submit">
-                        Login
+                        Entrar
                     </Button>
                 </Form>
             </Card.Body>
--- src/components/User/RegisterUser.jsx
+++ src/components/User/RegisterUser.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {Card, Col, Row, Container, Form, Button} from "react-bootstrap"
 
-const RegisterUser = () => {
+function RegisterUser (){
+
+    const [userName, setUserName] =  useState()
+    const [userEmail, setUserEmail] =  useState()
+    const [userPassword, setUserPassword] =  useState()
+
+
+    async function handleRegistar (e){
+        e.preventDefault()
+        alert(userName)
+
+        const response = await fetch('http://localhost:3000/auth/register',{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                userName,
+                userEmail,
+                userPassword
+            }),
+            
+        })
+
+        const data = await response.json()
+        console.log(data)
+    }
 
     return ( <>
     
@@ -15,18 +41,21 @@ const RegisterUser = () => {
                 <Card.Title>Registar Usuário</Card.Title>
             </Card.Header>
             <Card.Body>
-                <Form>
+                <Form onSubmit={handleRegistar}>
                     <Form.Control
-                    autoComplete="false"
-                    placeholder="Nome"/>
+                        autoComplete="false"
+                        onChange={(e)=>{setUserName(e.target.value)}}
+                        placeholder="Nome"/>
                     <br/>
                     <Form.Control 
-                    autoComplete="off"
-                    placeholder="E-mail"/>
+                        autoComplete="off"
+                        onChange={(e)=>{setUserEmail(e.target.value)}}
+                        placeholder="E-mail"/>
                     <br/>
                     <Form.Control
-                    placeholder="senha"
-                    type="password"/>
+                        placeholder="senha"
+                        onChange={(e)=>{setUserPassword(e.target.value)}}
+                        type="password"/>
                     <br/>
                     <Button variant="success" type="submit">
                         Registrar
