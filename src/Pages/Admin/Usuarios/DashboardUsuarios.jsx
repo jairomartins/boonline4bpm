197e54b6ba8cf74b77381b65ba738b9ec0ffc194
jmartins <jairo7martins@gmail.com>
2025-09-09 20:18:07 -0300

    edicao de usuario

--- src/Pages/Admin/Usuarios/DashboardUsuarios.jsx
+++ src/Pages/Admin/Usuarios/DashboardUsuarios.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Row} from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import Cabecalho from "../../../components/Cabecalho/Cabecalho";
 import FormBuscarUsuario from "../../../components/Form/FormBuscarUsuario";
 import axios from "axios";
@@ -9,127 +9,126 @@ import { useNavigate } from "react-router-dom";
 import { BiArrowBack, BiDownload, BiPlus } from "react-icons/bi";
 import EditarUsuario from "./EditarUsuario";
 
-const PROTOCOLO = process.env.REACT_APP_PROTOCOLO
-const API_PORT = process.env.REACT_APP_API_PORT
-const BASE_URL = process.env.REACT_APP_BASE_URL
-
+// URL da API a partir das variáveis de ambiente
+const API_URL = process.env.REACT_APP_API_URL;
 
 const DashboardUsuarios = () => {
+  const [usuarioList, setUsuarioList] = useState([]);
+  const [exibirFormCadastro, setExibirFormCadastro] = useState(false);
+  const [exibirFormEdicao, setExibirFormEdicao] = useState(false);
+  const [exibirTabelaUsuarios, setExibirTabelaUsuarios] = useState(false);
+  const [idBusca, setIdBusca] = useState();
+  const [index, setIndex] = useState();
 
-    const [usuarioList, setUsuarioList] = useState([])
-    const [exibirFormCadastro, setExibirFormCadastro] = useState(false)
-    const [exibirFormEdicao, setExibirFormEdicao] = useState(false)
-    const [exibirTabelaUsuarios,setExibirTabelaUsuarios] = useState(false)
-    const [idBusca, setIdBusca] = useState()
-    const [index, setIndex]= useState()
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()    
+  const handleClickBuscarUsuarios = () => {
+    setExibirFormCadastro(false);
+    setExibirFormEdicao(false);
 
-    const handleClickBuscarUsuarios = ()=>{
-        setExibirFormCadastro(false)
-        setExibirFormEdicao(false)
-        axios.get(`${PROTOCOLO}://${BASE_URL}:${API_PORT}/users/`,{
-            headers:{
-                "x-access-token":localStorage.getItem("x-access-token")
-            }
-        })
-        .then((response)=>{
-            console.log(response.data)
-            setExibirTabelaUsuarios(true)
-            setUsuarioList(response.data)
-        }).catch(function (error) {
-            console.error(error)
-        })
-        .then(function () {
-            // sempre será executado
-        });
-    }
+    axios
+      .get(`${API_URL}/users`, {
+        headers: {
+          "x-access-token": localStorage.getItem("x-access-token"),
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setExibirTabelaUsuarios(true);
+        setUsuarioList(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    const handleClickBuscarUsuario = ()=>{
-        setExibirFormEdicao(false)
-        setExibirFormCadastro(false)
-        axios.get(`${PROTOCOLO}://${BASE_URL}:${API_PORT}/user/${idBusca}`,{
-            headers:{
-                "x-access-token":localStorage.getItem("x-access-token")
-            }
-        })
-        .then((response)=>{
-            console.log(response.data[0])
-            setUsuarioList(response.data)
-        }).catch(function (error) {
-            console.error(error)
-        })
-        .then(function () {
-            // sempre será executado
-        });
-    }
+  const handleClickBuscarUsuario = () => {
+    setExibirFormEdicao(true);
+    setExibirFormCadastro(false);
 
-    const handleClickEditarUsuario = (index)=>{
-        setIndex(index)
-        setExibirTabelaUsuarios(false)
-        setExibirFormEdicao(true)
-        console.log("Editando ..."+ index)
-    }
+    axios
+      .get(`${API_URL}/users/${idBusca}`, {
+        headers: {
+          "x-access-token": localStorage.getItem("x-access-token"),
+        },
+      })
+      .then((response) => {
+        console.log(response.data[0]);
+        setUsuarioList(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    const handleClickRemoverUsuario = ()=>{
-        console.log("Removendo ...")
-    }
+  const handleClickEditarUsuario = (index) => {
+    setIndex(index);
+    setExibirTabelaUsuarios(false);
+    setExibirFormEdicao(true);
+    console.log("Editando ..." + index);
+  };
 
-    const handleClickRegistrarUsuarios = ()=>{
-        setUsuarioList([])
-        setExibirFormCadastro(true)
-    }
+  const handleClickRemoverUsuario = () => {
+    console.log("Removendo ...");
+  };
 
-    const handleClickVoltar = ()=>{
-        navigate("/dashboard")
-    }
+  const handleClickRegistrarUsuarios = () => {
+    setUsuarioList([]);
+    setExibirFormCadastro(true);
+  };
 
-    return ( 
+  const handleClickVoltar = () => {
+    navigate("/dashboard");
+  };
+
+  return (
     <>
-        <Cabecalho texto={"Gerenciador de Boletins Digitais: versão(2022.1)"}/>
+      <Cabecalho texto={"Gerenciador de Boletins Digitais: versão(2022.1)"} />
 
-        <Container>
-        
-           <h1>Gerenciamento de Usuários</h1>
-           <FormBuscarUsuario setIdBusca={setIdBusca} handleClickBuscarUsuarios={handleClickBuscarUsuario} />
-            <Row>
-                <Col className="d-grid gap-2 d-md-block mt-2 mb-2">
-                    <Button size="sm" variant="warning" onClick={handleClickBuscarUsuarios}>
-                        Carregar Usuários <BiDownload/>
-                    </Button>
-                    {" "}
-                    <Button size="sm"  variant="success" onClick={handleClickRegistrarUsuarios}>
-                        Cadastrar Usuário <BiPlus/>
-                    </Button>
-                    {" "}
-                    <Button size="sm"  variant="outline-primary" onClick={handleClickVoltar}>
-                        Voltar <BiArrowBack/>
-                    </Button>
-                </Col>
-            </Row>
-            { 
-                exibirTabelaUsuarios? 
-                
-                <TabelaUsuarios usuarioList={usuarioList} handleClickEditarUsuario={handleClickEditarUsuario} handleClickRemoverUsuario={handleClickRemoverUsuario} setIdBusca={idBusca}/>
+      <Container>
+        <h1>Gerenciamento de Usuários</h1>
+        <FormBuscarUsuario
+          setIdBusca={setIdBusca}
+          handleClickBuscarUsuarios={handleClickBuscarUsuario}
+        />
+        <Row>
+          <Col className="d-grid gap-2 d-md-block mt-2 mb-2">
+            <Button size="sm" variant="warning" onClick={handleClickBuscarUsuarios}>
+              Carregar Usuários <BiDownload />
+            </Button>{" "}
+            <Button size="sm" variant="success" onClick={handleClickRegistrarUsuarios}>
+              Cadastrar Usuário <BiPlus />
+            </Button>{" "}
+            <Button size="sm" variant="outline-primary" onClick={handleClickVoltar}>
+              Voltar <BiArrowBack />
+            </Button>
+          </Col>
+        </Row>
 
-                
-                : ""
-            }
+        {exibirTabelaUsuarios ? (
+          <TabelaUsuarios
+            usuarioList={usuarioList}
+            handleClickEditarUsuario={handleClickEditarUsuario}
+            handleClickRemoverUsuario={handleClickRemoverUsuario}
+            setIdBusca={idBusca}
+          />
+        ) : (
+          ""
+        )}
 
-            { 
-                exibirFormCadastro?  <CadastroUsuario/> :""
-            
-            }
-            { 
-                exibirFormEdicao?  <EditarUsuario usuario={usuarioList[index]} setExibirFormEdicao={setExibirFormEdicao} /> :""
-            
-            }
+        {exibirFormCadastro ? <CadastroUsuario /> : ""}
 
-            
-            
-        </Container>
+        {exibirFormEdicao ? (
+          <EditarUsuario
+            usuario={usuarioList[index]}
+            setExibirFormEdicao={setExibirFormEdicao}
+          />
+        ) : (
+          ""
+        )}
+      </Container>
+    </>
+  );
+};
 
-    </> );
-}
- 
 export default DashboardUsuarios;
--- src/Pages/Admin/Usuarios/EditarUsuario.jsx
+++ src/Pages/Admin/Usuarios/EditarUsuario.jsx
@@ -1,171 +1,185 @@
-import React, {useState} from "react"
-import { useNavigate } from 'react-router-dom'
-import axios from "axios"
-import InputMask from 'react-input-mask'
-import {Card, Form, Button} from "react-bootstrap"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import InputMask from "react-input-mask";
+import { Card, Form, Button } from "react-bootstrap";
 
+const API_URL = process.env.REACT_APP_API_URL;
 
+const EditarUsuario = ({ usuario, setExibirFormEdicao }) => {
+  const [userName, setUserName] = useState(usuario?.userName);
+  const [userEmail, setUserEmail] = useState(usuario?.userEmail);
+  const [userPassword, setUserPassword] = useState(usuario?.userPassword);
+  const [userContato, setUserContato] = useState(usuario?.userContato);
+  const [userMatriculaId, setUserMatriculaId] = useState(usuario?.userMatriculaId);
+  const [userTipo, setUserTipo] = useState(usuario?.tipo);
 
-const EditarUsuario = ({usuario, setExibirFormEdicao})=>{
+  const navigate = useNavigate();
 
-    const [userName, setUserName] =  useState(usuario?.userName)
-    const [userEmail, setUserEmail] =  useState(usuario?.userEmail)
-    const [userPassword, setUserPassword] =  useState(usuario?.userPassword)
-    const [userContato, setUserContato] =  useState(usuario?.userContato)
-    const [userMatriculaId, setUserMatriculaId] = useState(usuario?.userMatriculaId)
-    const [userTipo, setUserTipo] = useState(usuario?.tipo)
+  function limpaCampos() {
+    setUserName("");
+    setUserEmail("");
+    setUserPassword("");
+    setUserContato("");
+    setUserMatriculaId("");
+    setUserTipo("Comum");
+  }
 
-    const navigate = useNavigate()
+  async function handleUpdate(e) {
+    e.preventDefault();
 
-    const PROTOCOLO = process.env.REACT_APP_PROTOCOLO
-    const API_PORT = process.env.REACT_APP_API_PORT
-    const BASE_URL = process.env.REACT_APP_BASE_URL
+    axios
+      .put(
+        `${API_URL}/users/${usuario.userMatriculaId}`,
+        {
+          userName: userName,
+          userEmail: userEmail,
+          userPassword: userPassword,
+          userContato: userContato,
+          userMatriculaId: userMatriculaId,
+          tipo: userTipo,
+        },
+        {
+          headers: {
+            "x-access-token": localStorage.getItem("x-access-token"),
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response.data);
+        alert(response.data.message);
+        limpaCampos();
+        setExibirFormEdicao(false);
+        navigate("/administrador/usuarios");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
+  async function handleUpdatePassword(e) {
+    e.preventDefault();
+    console.log("editando senha");
 
-    function limpaCampos(){
-        setUserName("")
-        setUserEmail("")
-        setUserPassword("")
-        setUserContato("")
-        setUserMatriculaId("")
-        setUserTipo("Comum")
-    }
-    //Faz a conexão via axios para registrar o usuario
-    //Depois de registrado redireciona para tela de login
- 
-    async function handleUpdate (e){
-        
-        e.preventDefault()
+    axios
+      .post(
+        `${API_URL}/users/recoverPassword/${usuario?._id}`,
+        {
+          userPassword: userPassword,
+        },
+        {
+          headers: {
+            "x-access-token": localStorage.getItem("x-access-token"),
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response.data);
+        alert(response.data.message);
+        limpaCampos();
+        setExibirFormEdicao(false);
+        navigate("/administrador/usuarios");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-        axios.put(`https://${BASE_URL}:${API_PORT}/users/${usuario.userMatriculaId}`,{
-            userName:userName,
-            userEmail:userEmail,
-            userPassword:userPassword,
-            userContato:userContato,
-            userMatriculaId: userMatriculaId,
-            tipo:userTipo
-        },{
-            headers :{
-                "x-access-token":localStorage.getItem("x-access-token")
-            },
-        })
-        .then(function (response){
-            console.log(response.data)
-            alert(response.data.message)
-            limpaCampos()
-            setExibirFormEdicao(false)
-            navigate('/administrador/usuarios')
-
-        }).catch(function(error){
-            console.log(error)         
-
-        })
-    }
-    async function handleUpdatePassword (e){
-        
-        e.preventDefault()
-        console.log("editando senha")
-        axios.post(`${PROTOCOLO}://${BASE_URL}:${API_PORT}/recoverPassword/${usuario?._id}`,{
-            userPassword:userPassword
-        },{
-            headers :{
-                "x-access-token":localStorage.getItem("x-access-token")
-            },
-        })
-        .then(function (response){
-            console.log(response.data)
-            alert(response.data.message)
-            limpaCampos()
-            setExibirFormEdicao(false)
-            navigate('/administrador/usuarios')
-
-        }).catch(function(error){
-            console.log(error)         
-
-        })
-    }
-    return (
-        <>
-        <Card>
-            <Card.Header className="text-center">
-                <Card.Title>Editar Usuário</Card.Title>
-            </Card.Header>
-            <Card.Body>
-                <Form onSubmit={handleUpdate}>
-                    <Form.Label>Matrícula ou ID:</Form.Label>
-                    <InputMask 
-                        disabled
-                        required
-                        value={userMatriculaId}
-                        className="form-control "
-                        type="number"
-                        onChange={(e)=>{setUserMatriculaId(e.target.value)}}
-                        placeholder="Mátricula ou ID"
-                        />
-                    <br/>
-                    <Form.Label>Tipo:</Form.Label>
-                    <Form.Select onChange={(e)=>setUserTipo(e.target.value)}>
-                        <option value="Comum" selected={userTipo === "Comum"}>Comum</option>
-                        <option value="Copom" selected={userTipo === "Copom"}>Copom</option>
-                        <option value="P3" selected={userTipo === "P3"}>P3</option>
-                        <option value="admin" selected={userTipo === "admin"}>Admin</option>
-                    </Form.Select>
-                    <br/>
-                    <Form.Label>Usuário:</Form.Label>
-                    <Form.Control
-                        required
-                        value={userName}
-                        onChange={(e)=>{setUserName(e.target.value)}}
-                        placeholder="Nome"/>
-                    <br/>
-                    
-                    <Form.Label>E-mail:</Form.Label>
-                    <Form.Control 
-                        required
-                        value={userEmail}
-                        onChange={(e)=>{setUserEmail(e.target.value)}}
-                        placeholder="E-mail"/>
-                    <br/>
-                    <Form.Label>Contato:</Form.Label>
-                    <InputMask 
-                        required
-                        value={userContato}
-                        className="form-control "
-                        mask="(99) 9 9999-9999"
-                        onChange={(e)=>{setUserContato(e.target.value)}}
-                        placeholder="(99) 9 0000-0000"
-                        />
-                    <br/>                  
-                    <Button variant="warning" type="submit">
-                        Salvar Alteraçoes
-                    </Button>
-                </Form>
-                
-            </Card.Body>
-        </Card>
-        <br/>
-        <Card>
+  return (
+    <>
+      <Card>
         <Card.Header className="text-center">
-            <Card.Title>Alterar Senha</Card.Title>
+          <Card.Title>Editar Usuário</Card.Title>
         </Card.Header>
         <Card.Body>
-            <Form onSubmit={handleUpdatePassword}>
-            
-                <Form.Control
-                    required
-                    placeholder="senha"
-                    onChange={(e)=>{setUserPassword(e.target.value)}}
-                    type="password"/>
-                <br/> 
+          <Form onSubmit={handleUpdate}>
+            <Form.Label>Matrícula ou ID:</Form.Label>
+            <InputMask
+              disabled
+              required
+              value={userMatriculaId}
+              className="form-control "
+              type="number"
+              onChange={(e) => {
+                setUserMatriculaId(e.target.value);
+              }}
+              placeholder="Mátricula ou ID"
+            />
+            <br />
+            <Form.Label>Tipo:</Form.Label>
+            <Form.Select
+              value={userTipo}
+              onChange={(e) => setUserTipo(e.target.value)}
+            >
+              <option value="Comum">Comum</option>
+              <option value="Copom">Copom</option>
+              <option value="P3">P3</option>
+              <option value="admin">Admin</option>
+            </Form.Select>
+            <br />
+            <Form.Label>Usuário:</Form.Label>
+            <Form.Control
+              required
+              value={userName}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+              placeholder="Nome"
+            />
+            <br />
 
-                <Button variant="warning" type="submit">
-                    Confirmar
-                </Button>
-            </Form>
-            
+            <Form.Label>E-mail:</Form.Label>
+            <Form.Control
+              required
+              value={userEmail}
+              onChange={(e) => {
+                setUserEmail(e.target.value);
+              }}
+              placeholder="E-mail"
+            />
+            <br />
+            <Form.Label>Contato:</Form.Label>
+            <InputMask
+              required
+              value={userContato}
+              className="form-control "
+              mask="(99) 9 9999-9999"
+              onChange={(e) => {
+                setUserContato(e.target.value);
+              }}
+              placeholder="(99) 9 0000-0000"
+            />
+            <br />
+            <Button variant="warning" type="submit">
+              Salvar Alterações
+            </Button>
+          </Form>
         </Card.Body>
-        </Card>
-        </>
-    )
-}
-export default EditarUsuario
+      </Card>
+      <br />
+      <Card>
+        <Card.Header className="text-center">
+          <Card.Title>Alterar Senha</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleUpdatePassword}>
+            <Form.Control
+              required
+              placeholder="senha"
+              onChange={(e) => {
+                setUserPassword(e.target.value);
+              }}
+              type="password"
+            />
+            <br />
+            <Button variant="warning" type="submit">
+              Confirmar
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default EditarUsuario;
