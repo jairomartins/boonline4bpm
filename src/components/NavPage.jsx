c4a35eabcb599ac244c393e10b1796a0487dc0ac
jmartins <jairo7martins@gmail.com>
2025-10-08 00:55:25 -0300

    navegacao estilo

--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -2,38 +2,41 @@ import React from "react";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
+import "./NavPage.css";
 
 const NavPage = ({ prev, next }) => {
   return (
-    <Container className="my-4">
-      <Row className="g-3 justify-content-center">
-        {/* Botão Voltar */}
-        <Col xs={12} sm={6} md={4} className="d-grid">
-          <Button
-            as={Link}
-            to={prev}
-            variant="outline-primary"
-            className="fw-semibold py-2"
-          >
-            <BsArrowLeft className="me-2" />
-            Voltar
-          </Button>
-        </Col>
+    <div className="navpage-wrapper">
+      <Container fluid className="navpage-container">
+        <Row className="g-3 justify-content-center align-items-center">
+          {/* Botão Voltar */}
+          <Col xs={12} sm={6} md={4} className="d-grid">
+            <Button
+              as={Link}
+              to={prev}
+              variant="light"
+              className="nav-btn nav-btn-prev"
+            >
+              <BsArrowLeft className="me-2" />
+              Voltar
+            </Button>
+          </Col>
 
-        {/* Botão Próximo */}
-        <Col xs={12} sm={6} md={4} className="d-grid">
-          <Button
-            as={Link}
-            to={next}
-            variant="primary"
-            className="fw-semibold py-2"
-          >
-            Próximo
-            <BsArrowRight className="ms-2" />
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+          {/* Botão Próximo */}
+          <Col xs={12} sm={6} md={4} className="d-grid">
+            <Button
+              as={Link}
+              to={next}
+              variant="primary"
+              className="nav-btn nav-btn-next"
+            >
+              Próximo
+              <BsArrowRight className="ms-2" />
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
